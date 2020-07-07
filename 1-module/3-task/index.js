5699261806e924162ceb7043eee60c138d80be07
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 14:35:56 +0300

    Added solution for task: Сделать первый символ заглавным

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,9 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  let firstLetter = str.charAt(0);
+  let firstLetterUpperCase = firstLetter.toUpperCase();
+  let otherLetters = str.slice(1);
+
+  return `${firstLetterUpperCase}${otherLetters}`;
 }
