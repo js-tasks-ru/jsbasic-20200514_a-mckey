9c85b89f5d01d6d622e2d35dc60759603d72c952
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 14:54:07 +0300

    Added task Функция showSalary(data,age)

---
+++ 3-module/1-task/index.js
@@ -0,0 +1,8 @@
+/**
+ * showSalary
+ * @param {Array} data - данные о пользователях
+ * @param {number} age - максимальный возраст
+ * @returns {string}
+ */
+function showSalary(data, age) {
+}
---
+++ 3-module/1-task/task.test.js
@@ -0,0 +1,289 @@
+/* eslint-disable */
+const data = [
+  {
+    'balance': '$1,825.65',
+    'picture': 'http://placehold.it/32x32',
+    'age': 21,
+    'name': 'Golden Branch',
+    'gender': 'male',
+    'greeting': 'Hello, Golden Branch! You have 7 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    'index': 1,
+    'guid': '53c90e0c-b8b2-494b-8508-7d5870af0351',
+    'isActive': true,
+    'balance': '$1,490.15',
+    'picture': 'http://placehold.it/32x32',
+    'age': 26,
+    'eyeColor': 'green',
+    'name': 'Duncan Randall',
+    'gender': 'male',
+    'company': 'MATRIXITY',
+    'email': 'duncanrandall@matrixity.com',
+    'phone': '+1 (861) 585-3156',
+    'address': '860 Lamont Court, Bowden, South Carolina, 3719',
+    'about': 'Exercitation magna aliquip veniam exercitation esse est cillum non in. Dolor magna nostrud irure nulla elit anim fugiat et eiusmod. Elit reprehenderit mollit officia eiusmod consectetur id. Anim sunt deserunt aliqua eu sint mollit Lorem quis officia in aliquip dolore. Adipisicing ut ut aliquip est occaecat magna deserunt ad magna labore culpa velit sunt commodo. Nulla laboris irure amet voluptate sit excepteur mollit nostrud. Ullamco reprehenderit proident anim laborum dolore deserunt sunt nostrud deserunt in enim.\r\n',
+    'registered': '2015-03-09T02:11:47 -03:00',
+    'latitude': -36.819023,
+    'longitude': -6.01344,
+    'tags': [
+      'aliquip',
+      'esse',
+      'nisi',
+      'commodo',
+      'ad',
+      'velit',
+      'qui'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Townsend Lindsey'
+      },
+      {
+        'id': 1,
+        'name': 'Georgette Blackwell'
+      },
+      {
+        'id': 2,
+        'name': 'Knowles Cervantes'
+      }
+    ],
+    'greeting': 'Hello, Duncan Randall! You have 2 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    'index': 2,
+    'guid': '2483e252-b0d2-4a26-98e6-513531b320b7',
+    'isActive': true,
+    'balance': '$1,664.27',
+    'picture': 'http://placehold.it/32x32',
+    'age': 37,
+    'eyeColor': 'brown',
+    'name': 'Glass Ray',
+    'gender': 'male',
+    'company': 'ANIVET',
+    'email': 'glassray@anivet.com',
+    'phone': '+1 (953) 592-3001',
+    'address': '864 Driggs Avenue, Newkirk, New Jersey, 4485',
+    'about': 'Laboris ex mollit ut excepteur qui adipisicing magna eu anim nulla anim sint sunt officia. Commodo nisi cupidatat aliquip laboris consectetur et commodo veniam nisi sunt nostrud culpa ipsum laborum. Tempor aliquip sint amet culpa.\r\n',
+    'registered': '2016-07-16T09:20:34 -03:00',
+    'latitude': 76.633104,
+    'longitude': 1.779366,
+    'tags': [
+      'voluptate',
+      'excepteur',
+      'fugiat',
+      'aute',
+      'dolore',
+      'aliqua',
+      'minim'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Simmons Grimes'
+      },
+      {
+        'id': 1,
+        'name': 'Chandra Vasquez'
+      },
+      {
+        'id': 2,
+        'name': 'Jimenez Hayes'
+      }
+    ],
+    'greeting': 'Hello, Glass Ray! You have 8 unread messages.',
+    'favoriteFruit': 'strawberry'
+  },
+  {
+    'index': 3,
+    'guid': '51855992-a0e7-4821-8178-92bcd13a1f0c',
+    'isActive': true,
+    'balance': '$1,072.95',
+    'picture': 'http://placehold.it/32x32',
+    'age': 40,
+    'eyeColor': 'brown',
+    'name': 'Helga Nixon',
+    'gender': 'female',
+    'company': 'KENEGY',
+    'email': 'helganixon@kenegy.com',
+    'phone': '+1 (832) 595-2918',
+    'address': '658 Scholes Street, Day, Illinois, 3262',
+    'about': 'Labore id aute id voluptate do sint ex veniam dolor do non velit. Proident consectetur ipsum laboris ea aliquip ea. Incididunt ut labore incididunt sunt ipsum ipsum Lorem quis proident nulla sit cillum duis. Veniam labore aliqua in elit aute. Pariatur deserunt dolore fugiat mollit. Excepteur ea nostrud aliquip officia fugiat. Occaecat elit elit consectetur et commodo aliqua reprehenderit eu nulla excepteur.\r\n',
+    'registered': '2015-09-27T01:16:15 -03:00',
+    'latitude': 63.00501,
+    'longitude': -27.098594,
+    'tags': [
+      'consequat',
+      'reprehenderit',
+      'quis',
+      'esse',
+      'sint',
+      'dolore',
+      'amet'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Andrews Sharp'
+      },
+      {
+        'id': 1,
+        'name': 'Leona Jacobson'
+      },
+      {
+        'id': 2,
+        'name': 'Marie Parker'
+      }
+    ],
+    'greeting': 'Hello, Helga Nixon! You have 3 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    'index': 4,
+    'guid': '83405931-c746-4ed6-a05f-10b79b018af2',
+    'isActive': false,
+    'balance': '$3,422.96',
+    'picture': 'http://placehold.it/32x32',
+    'age': 45,
+    'eyeColor': 'blue',
+    'name': 'Felicia Browning',
+    'gender': 'female',
+    'company': 'TRASOLA',
+    'email': 'feliciabrowning@trasola.com',
+    'phone': '+1 (947) 523-3902',
+    'address': '561 Rost Place, Tryon, District Of Columbia, 1276',
+    'about': 'Elit commodo adipisicing duis voluptate ut ad. Occaecat consectetur amet do qui cupidatat anim aliqua laborum culpa quis id officia. Non aute irure reprehenderit officia id aliquip adipisicing non aliqua voluptate fugiat nulla dolor cillum. Commodo cillum id irure magna ut commodo aliqua in velit tempor pariatur voluptate nulla. Laborum consectetur magna mollit fugiat ad fugiat velit adipisicing qui et qui amet. Do esse enim ullamco excepteur eiusmod ipsum deserunt id consectetur deserunt anim amet.\r\n',
+    'registered': '2014-11-12T03:57:54 -03:00',
+    'latitude': -19.354794,
+    'longitude': 125.464668,
+    'tags': [
+      'tempor',
+      'sunt',
+      'consectetur',
+      'est',
+      'minim',
+      'enim',
+      'cillum'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Hudson Hartman'
+      },
+      {
+        'id': 1,
+        'name': 'Marshall Hebert'
+      },
+      {
+        'id': 2,
+        'name': 'Rosario Benson'
+      }
+    ],
+    'greeting': 'Hello, Felicia Browning! You have 4 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    'index': 5,
+    'guid': 'd4f7c1c1-a902-431c-95ce-034f74cae5c7',
+    'isActive': false,
+    'balance': '$2,560.80',
+    'picture': 'http://placehold.it/32x32',
+    'age': 39,
+    'eyeColor': 'brown',
+    'name': 'Parrish Jordan',
+    'gender': 'male',
+    'company': 'PARCOE',
+    'email': 'parrishjordan@parcoe.com',
+    'phone': '+1 (975) 582-2005',
+    'address': '178 Franklin Street, Fredericktown, Missouri, 6785',
+    'about': 'Officia ex tempor est eiusmod aliquip Lorem. Veniam mollit anim pariatur consequat. Qui exercitation deserunt esse est.\r\n',
+    'registered': '2016-04-23T09:49:37 -03:00',
+    'latitude': 56.387518,
+    'longitude': -32.3259,
+    'tags': [
+      'pariatur',
+      'incididunt',
+      'labore',
+      'dolore',
+      'mollit',
+      'mollit',
+      'exercitation'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Rosa Manning'
+      },
+      {
+        'id': 1,
+        'name': 'Fernandez Leach'
+      },
+      {
+        'id': 2,
+        'name': 'Crawford Dejesus'
+      }
+    ],
+    'greeting': 'Hello, Parrish Jordan! You have 3 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    'index': 6,
+    'guid': '7f120395-5c8b-4fdc-9dd6-6892af61e28b',
+    'isActive': true,
+    'balance': '$2,461.96',
+    'picture': 'http://placehold.it/32x32',
+    'age': 55,
+    'eyeColor': 'green',
+    'name': 'Adela Wooten',
+    'gender': 'female',
+    'company': 'ANOCHA',
+    'email': 'adelawooten@anocha.com',
+    'phone': '+1 (991) 588-2830',
+    'address': '706 Will Place, Needmore, Montana, 9130',
+    'about': 'Fugiat ea irure nisi aute Lorem ad exercitation nostrud. Laborum anim ullamco deserunt non minim sunt mollit duis labore ad ex veniam mollit. Dolor elit culpa esse fugiat ea non amet minim consequat. Lorem esse elit aute dolor enim adipisicing commodo. Esse id enim proident incididunt non proident cillum officia elit reprehenderit consequat qui et ut.\r\n',
+    'registered': '2017-04-22T12:16:38 -03:00',
+    'latitude': 58.71205,
+    'longitude': 127.576842,
+    'tags': [
+      'esse',
+      'reprehenderit',
+      'voluptate',
+      'fugiat',
+      'pariatur',
+      'nisi',
+      'mollit'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Kendra Mcgowan'
+      },
+      {
+        'id': 1,
+        'name': 'Meghan Ramos'
+      },
+      {
+        'id': 2,
+        'name': 'Johns Rich'
+      }
+    ],
+    'greeting': 'Hello, Adela Wooten! You have 7 unread messages.',
+    'favoriteFruit': 'strawberry'
+  }
+];
+/* eslint-enable */
+
+describe('3-module-1-task', () => {
+  it('проверяем моложе или равному 30 лет', () => {
+    expect(showSalary(data, 30)).toEqual('Golden Branch, $1,825.65\nDuncan Randall, $1,490.15');
+  });
+
+  it('проверяем моложе или равному 40 лет', () => {
+    /* eslint-disable */
+    expect(showSalary(data, 40)).toEqual('Golden Branch, $1,825.65\nDuncan Randall, $1,490.15\nGlass Ray, $1,664.27\nHelga Nixon, $1,072.95\nParrish Jordan, $2,560.80');
+    /* eslint-enable */
+  });
+});
