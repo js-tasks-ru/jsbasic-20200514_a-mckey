a426c407e255bd33117e31dcd21813db0f40f5e5
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 15:04:43 +0300

    Added solution for task: Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,23 @@
  * @returns {string}
  */
 function camelize(str) {
+  if (!str) {
+    return str;
+  }
+
+  return str
+    .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+    .map((part, index) => {
+      // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+      // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+      if (index === 0) { // Это нужно делать, чтобы не делать заглавной первую часть
+        return part;
+      }
+
+      let firstLetter = part.slice(0, 1);
+      let rest = part.slice(1);
+
+      return `${firstLetter.toUpperCase()}${rest}`;
+    })
+    .join('');
 }
