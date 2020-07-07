6779160f13d1bfd6786ab5f3e24757eed79d856c
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 14:48:18 +0300

    Added solution for task: Усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,14 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+  let strLength = str.length;
+  let isExceedMaxlength = strLength > maxlength;
 
+  if (isExceedMaxlength) {
+    let shortenStr = str.slice(0, maxlength - 1);
+
+    return `${shortenStr}…`;
+  }
+
+  return str;
 }
