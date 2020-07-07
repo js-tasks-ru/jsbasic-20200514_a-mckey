61d1934aeee3aecfe73f87fca4da07d629597ce9
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 15:02:09 +0300

    Added solution for task: Функция showSalary(data,age)

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,8 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  return data
+    .filter(item => item.age <= age)
+    .map(item => `${item.name}, ${item.balance}`)
+    .join('\n');
 }
