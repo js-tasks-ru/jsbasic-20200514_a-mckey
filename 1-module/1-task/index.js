d140990b45d991388bf50fbe5ca2f87408642b5f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 14:17:14 +0300

    Added solution for task: Вычислить факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {number}
  */
 function factorial(n) {
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result;
+  }
 
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
 }
