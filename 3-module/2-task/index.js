a59134b6da36201d2534b57baa77849dd4a55967
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 15:04:33 +0300

    Added solution for task: Найти min/max в строке

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,12 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let segments = str.match(/(-|\+)?[\d]+(\.[\d]+)?/ig) || [];
+
+  segments = segments.map(seg => parseFloat(seg));
+
+  return {
+    min: Math.min(...segments),
+    max: Math.max(...segments),
+  };
 }
