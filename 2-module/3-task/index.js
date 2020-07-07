091ae59dc22363be14793de1a2b0a1e6a18cddba
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 14:46:53 +0300

    Added solution for task: Создайте калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
