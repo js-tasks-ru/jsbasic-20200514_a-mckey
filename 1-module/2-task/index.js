6ac46a514f6d9be185c692602403c50052f4ff3f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-07 14:07:58 +0300

    Added solution for task: Допишите функцию валидации имени пользователя

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (!name || name.indexOf(' ') !== -1 || name.length < 4) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
