59df9e559fd5db78bac71bee99d9f5468809033d
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-14 21:44:36 +0300

    Added solution for task: Компонент Очищаемая таблица (ClearedTable)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -17,10 +17,60 @@ class ClearedTable {
   constructor(data) {
     this.el = document.createElement('table');
     this.data = data;
+
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+
+    this.el.classList.toggle('pure-table');
+
+    thead.innerHTML = `
+             <tr>
+                <td>Name</td>
+                <td>Age</td>
+                <td>Salary</td>
+                <td>City</td>
+                <td></td>
+             </tr>
+        `;
+
+    tbody.innerHTML = data.map((item) => {
+      let row = '';
+
+      for (const key in item) {
+        if (key !== 'id') {
+          row += `<td>${item[key]}</td>`;
+        }
+      }
+
+      return `<tr>
+            ${row}
+            <td data-id="${item.id}"><a href="#delete">X</a></td>
+        </tr>`;
+    }).join('');
+
+    this.el.appendChild(thead);
+    this.el.appendChild(tbody);
+
+    this.el.addEventListener('click', event => this.onClick(event));
+  }
+
+  onClick(event) {
+    if (!(event.target instanceof HTMLAnchorElement)) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const tr = event.target.closest('tr');
+    const id = tr.querySelector('[data-id]').getAttribute('data-id');
+
+    tr.remove();
+
+    this.onRemoved(parseInt(id, 10));
   }
 
   /**
-   * Метод который вызывается после удалении строки
+   * Метод который выщывается после удалении строки
    * @param {number} id - идентификатор удаляемого пользователя
    */
   onRemoved(id) {}
