5dfb65ba79a931fecad37521b4f4ee89a2012a82
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-08-11 12:35:41 +0300

    Added solution for task: Функция highlight

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,6 +1,45 @@
+const FIRST_COLUMN = 1;
+const SECOND_COLUMN = 2;
+const THIRD_COLUMN = 3;
+
 /**
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
 function highlight(table) {
+  const actions = {
+    [THIRD_COLUMN]: (root, td) => {
+      if (td.dataset.available === 'true') {
+        root.classList.toggle('available', true);
+      } else if (td.dataset.available === 'false') {
+        root.classList.toggle('unavailable', true);
+      } else if (!td.hasAttribute('data-available')) {
+        root.hidden = true;
+      }
+    },
+    [SECOND_COLUMN]: (root, td) => {
+      if (td.textContent === 'm') {
+        root.classList.toggle('male', true);
+      } else if (td.textContent === 'f') {
+        root.classList.toggle('female', true);
+      }
+    },
+    [FIRST_COLUMN]: (root, td) => {
+      const age = parseInt(td.textContent, 10);
+
+      if (age < 18) {
+        root.style.textDecoration = 'line-through';
+      }
+    },
+  };
+
+  for (const tr of table.rows) {
+    Array.from(tr.cells).forEach((td, index) => {
+      const fn = actions[index];
+
+      if (typeof fn === 'function') {
+        fn(tr, td);
+      }
+    });
+  }
 }
