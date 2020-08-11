026433df0a18e2beac63b58735ef2583e575398a
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-08-11 12:34:50 +0300

    Added solution for task: Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,11 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  const ul = document.createElement('ul');
+
+  ul.innerHTML = friends.map(item => `
+      <li>${item.firstName} ${item.lastName}</li>
+  `).join('');
+
+  return ul;
 }
